799f9fbe1175bd8baa25a9ac43b85a708c3bb352
Sambaran Hazra <hazra@mediamarktsaturn.com>
2022-11-15 14:32:46 +0100

    using jackson kotlin which allows NPE to be propagated and flexible naming strategy

--- src/test/kotlin/JsonSerializationTest.kt
+++ src/test/kotlin/JsonSerializationTest.kt
@@ -1,39 +1,46 @@
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.MissingFieldException
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.io.File
 
-@OptIn(ExperimentalSerializationApi::class)
 class JsonSerializationTest {
 
-    private val json: Json = Json { explicitNulls = false }
+    private val mapper = ObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true)
+        .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+        .registerModule(KotlinModule.Builder().build())
 
     @Test
     fun `test for null de-serialization`() {
-        assertThrows<MissingFieldException> {
-            json.decodeFromString<Person>(File("src/test/resources/nullable_weight.json").readText(Charsets.UTF_8))
+        assertThrows<MismatchedInputException> {
+            mapper.readValue(
+                File("src/test/resources/nullable_weight.json").readText(Charsets.UTF_8),
+                Person::class.java
+            )
         }
     }
 
     @Test
     fun `test for not null de-serialization`() {
-        val person =
-            json.decodeFromString<Person>(File("src/test/resources/non_nullable_weight.json").readText(Charsets.UTF_8))
+        val person: Person =
+            mapper.readValue(
+                File("src/test/resources/non_nullable_weight.json").readText(Charsets.UTF_8),
+                Person::class.java
+            )
         assertNotNull(person)
         assertNull(person.age)
     }
 }
 
-@Serializable
 data class Person(
-    val name: String,
+    val fullName: String,
     val height: Int,
-    val weight: Int, // non nullable - should not allow missing value
+    val weight: Int, // non-nullable - should not allow missing value
     val age: Int? // nullable - should be null in case of no value
 )
